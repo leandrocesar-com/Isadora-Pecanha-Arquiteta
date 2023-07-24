600018f0e05d98fa15ad2bce657b3630fc0b661c
professorleandrocesar <professor@leandrocesar.co>
2023-07-24 00:11:57 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useHead({
+    titleTemplate: 'Isadora Peçanha - Arquiteta',
+})
+</script>
+
+<template>
+    <main>
+        <LogoHome />
+        <TheWelcome />
+        <NavBottom />
+    </main>
+</template>
